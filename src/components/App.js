a736b347d909f09030dd15048700cd00d6fdf0b7
arshad012 <forarshad99@gmail.com>
2025-01-21 23:44:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,10 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-    <h1>Memory Game</h1>
+    <h1>Welcome!</h1>
+    <input type='radio' id='easy' />
+    <input type='radio' id='medium' />
+    <input type='radio' id='hard' />
     </div>
   )
 }
